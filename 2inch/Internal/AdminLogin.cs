3e7a298be9d84527f86971595e7fbcf9ba8d6b05
andrejmokris <mokris.andrej@gmail.com>
2020-07-28 18:38:17 +0200

    Admin Page update

--- 2inch/Internal/AdminLogin.cs
+++ 2inch/Internal/AdminLogin.cs
@@ -9,11 +9,11 @@ namespace _2inch.Internal
     {
         public static async Task<bool> CheckCred(Models.Auth auth) //auth should contain Name and Pass
         { //Static je tu zatial len preto, aby mi nevyhadzoval refference error.
-            var TestCheck = true; //TOTO JE LEN EXAMPLE
-            var result = TestCheck; //TOTO JE LEN EXAMPLE
+            //var TestCheck = true; //TOTO JE LEN EXAMPLE
+            //var result = TestCheck; //TOTO JE LEN EXAMPLE
             //Treba tu pridat takyto test:
             //If the Credentials are in the db, result = true, if not, result = false
-            return result;
+            return false;
         }
     }
 }
