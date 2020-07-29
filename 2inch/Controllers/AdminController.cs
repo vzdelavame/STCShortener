ecdeefdb266cdd825631364832c80aac44d5abdf
andrejmokris <mokris.andrej@gmail.com>
2020-07-29 16:13:31 +0200

    Adding links into database works, todo: Auth

--- 2inch/Controllers/AdminController.cs
+++ 2inch/Controllers/AdminController.cs
@@ -36,6 +36,15 @@ namespace _2inch.Controllers
             if (await Database.VerifyAdminCredentials(auth)) return View("AdminPanel"); //Redirects to AdminPanel if returns True
             ViewBag.Passed = false; //Should Update Admin login with 'Incorrect Credentials'
             return View(); //Redirection to the same page if AdminLog.CheckCred(auth) returns False
-        } 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddLinks(Models.Link newlink)
+        {
+            newlink.createdBy = "Testik";
+            newlink.clicked = 0;
+            await Database.InsertLink(newlink); 
+            return View("AdminPanel");
+        }
     }
 }
